32e9bd208b817318b220b66dd0ac9065d799add6
Jesus Flores <joy_warmgun@hotmail.com>
2015-11-18 22:16:25 -0600

    Reestructurando 	new file:   UCRest/app/Models/Entity/BaseEntity.php 	new file:   UCRest/app/Models/Entity/TestEntity.php 	deleted:    UCRest/app/Models/Layers/Entities/TipoUsuario.php 	deleted:    UCRest/app/Models/Layers/EntityFactory.php

---
+++ UCRest/app/Models/Entity/BaseEntity.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace UCRest\Models\Entity;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+* 
+*/
+class BaseEntity extends Model
+{
+
+}
---
+++ UCRest/app/Models/Entity/TestEntity.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace UCRest\Models\Entity;
+
+/**
+* 
+*/
+class TestEntity extends BaseEntity
+{
+	
+	
+}
